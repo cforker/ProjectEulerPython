a2cc224c37465a07cc7bd50b0b503a9fb04ec94a
Chris Forker <cforker@gmail.com>
2013-04-26 23:35:00 -0700

    Initial commit
    No functionality yet.  Prints consecutive binary numbers padded to 8
    digits.  Using Eclipse with PyDev to develop.

---
+++ PE300.py
@@ -0,0 +1,20 @@
+'''
+Created on Apr 26, 2013
+
+@author: cforker
+'''
+from itertools import count
+
+def leadingZeroes(binstring,length):
+    return '0'*(length-len(binstring[2:])) + binstring[2:]
+
+if __name__ == '__main__':
+    N = 8
+    # binary representation of proteins. 1=H,0=P
+    for prot in count():
+        if (prot == 2**N):
+            break
+        sprot = leadingZeroes(bin(prot),N)
+        print sprot
+          
+        
